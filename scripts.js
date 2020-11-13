dd714597d2bba7a551484fd7b416b912b9bbf7d3
cdmlclm <cdmlclm@hotmail.com>
2020-11-12 23:59:38 -0500

    Implement most of the color parsing and calculation

--- scripts.js
+++ scripts.js
@@ -125,18 +125,77 @@ function applyToggleBorder() {
   currentToggle.setAttribute('style', 'margin: 0px; border: 3px solid #808080;')
 }
 
+function parseColor(c) {
+  return [parseInt(c.slice(1,3), 16), parseInt(c.slice(3, 5), 16), parseInt(c.slice(5), 16)];
+}
+
+function parseRGB(c) {
+  let arr = c.split(',');
+  return [parseInt(arr[0].slice(4)), parseInt(arr[1].slice(1)), parseInt(arr[2].slice(1))];
+}
+
+function randColor() {
+  return `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+}
+
+function parseDiffs(oldColor) {
+  const bgVals = parseRGB(oldColor);
+  const penVals = parseColor(penColor);
+
+  return [penVals[0]-bgVals[0], penVals[1]-bgVals[1], penVals[2]-bgVals[2]];
+}
+
+function comp(d, a, b) {
+  if (a == b) return a;
+
+  if (a < b) {
+    return Math.min(Math.floor(a+d/10), b);
+  } else {
+    return Math.max(Math.ceil(a+d/10), b);
+  }
+}
+
+function sameColor(a, b) {
+  for (let i = 0; i < a.length; i++) if (a[i] != b[i]) return false;
+  return true;
+}
+
 function getColor(oldColor) {
   if (penType == 1 && currentToggle === solidToggle) return bgColor;
 
   if (currentToggle === solidToggle) {
     if (currentBrush !== rainbowBrush) return penColor;
-    return `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+    return randColor();
   }
 
-  // black // write // shader
+  if (penType == 1) {
+    // TODO: Deal with this
+    return bgColor;
+  }
+
+  if (currentBrush !== rainbowBrush) {
+    const diffs = parseDiffs(oldColor);
+    const oldVals = parseRGB(oldColor);
+    const penVals = parseColor(penColor);
+
+    const r = comp(diffs[0], oldVals[0], penVals[0]);
+    const g = comp(diffs[1], oldVals[1], penVals[1]);
+    const b = comp(diffs[2], oldVals[2], penVals[2]);
+
+    console.log([r,g,b]);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  console.log(parseRGB(oldColor));
+  console.log(parseColor(bgColor));
+  if (sameColor(parseRGB(oldColor), parseColor(bgColor))) {
+    return randColor();
+  }
+
+  // need to refactor parsediffs to handle this
+
   // black // eraser // shader
 
-  // other // write // shader
   // other // eraser // shader
 
   // rando // write // shader
