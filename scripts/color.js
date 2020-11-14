1abdb297c29fea6221a42c98808d1a7417b11ed6
cdmlclm <cdmlclm@hotmail.com>
2020-11-13 22:55:26 -0500

    Final refactor and comments

--- scripts/color.js
+++ scripts/color.js
@@ -1,36 +1,48 @@
 export class Color {
+  // args can be String or Int, Int, Int
   constructor(...args) {
     if (args.length == 1) {
+      // String is either of the form "#xxxxxx" or "rgb(r,g,b)"
       const c = args[0];
-      if (c.charAt(0) == '#') {
+
+      if (c.charAt(0) == '#') { // hex format
         this.r = parseInt(c.slice(1, 3), 16);
         this.g = parseInt(c.slice(3, 5), 16);
         this.b = parseInt(c.slice(5), 16);
-      } else {
+      } else {  // rgb format
         const arr = c.split(',');
         this.r = parseInt(arr[0].slice(4));
         this.g = parseInt(arr[1].slice(1));
         this.b = parseInt(arr[2].slice(1));
       }
     } else {
+      // args will be r, g, b, in that order
       this.r = args[0];
       this.g = args[1];
       this.b = args[2];
     }
   }
 
+  // null -> int
+  // generate a random integer in the range [0, 255]
   static getRandNum() {
     return Math.floor(Math.random()*256);
   }
 
+  // null -> Color
+  // generate a random Color
   static getRandColor() {
-    return `rgb(${Color.getRandNum()}, ${Color.getRandNum()}, ${Color.getRandNum()})`;
+    return new Color(Color.getRandNum(), Color.getRandNum(), Color.getRandNum());
   }
 
+  // int int -> int
+  // given two numbers, returns a number 10% closer to the second number than the first
   static getCloserPart(a, b) {
     return (a < b) ? Math.ceil(a + (b-a)/10) : Math.floor(a + (b-a)/10);
   }
 
+  // Color Color -> Color
+  // given another Color, returns a Color 10% closer to the other Color than "this" Color
   getCloserColor(other) {
     const newRed = Color.getCloserPart(this.r, other.r);
     const newGreen = Color.getCloserPart(this.g, other.g);
@@ -39,14 +51,20 @@ export class Color {
     return new Color(newRed, newGreen, newBlue);
   }
 
+  // Color Color -> boolean
+  // checks if two colors are the same
   checkIfSame(other) {
     return (this.r == other.r && this.g == other.g && this.b == other.b);
   }
 
+  // Color -> String
+  // returns a rgb-fomratted string based on the values of "this"
   toString() {
     return `rgb(${this.r}, ${this.g}, ${this.b})`;
   }
 
+  // Color -> Color
+  // returns the complementary color of "this"
   getComplement() {
     return new Color(255-this.r, 255-this.g, 255-this.b);
   }
--- scripts/main.js
+++ scripts/main.js
@@ -1,103 +1,125 @@
 import { Color } from './color.js';
 
-const blackBrush = document.getElementById('black-toggle');
-const colorBrush = document.getElementById('color-toggle');
-const rainbowBrush = document.getElementById('rainbow-toggle');
-const brushes = [blackBrush, colorBrush, rainbowBrush];
+// ----------------- Constant Definitions---------------------------------------
+const BLACK = '#000000';
+const GREY = '#808080';
+const BLACK_COLOR = new Color(0,0,0);
 
-const solidToggle = document.getElementById('solid-toggle');
-const dragToggle = document.getElementById('drag-toggle');
-
-const dottedGridToggle = document.getElementById('dotted-gridlines');
-const solidGridToggle = document.getElementById('solid-gridlines');
-
-const pens = document.querySelectorAll('.pen-setting');
-
+// ----------------- DOM Elements ----------------------------------------------
 const colorBrushInput = document.getElementById('brush-color');
 const colorBackgroundInput = document.getElementById('bg-color');
-
-const resetButton = document.getElementById('reset-button');
-const applyButton = document.getElementById('apply-button');
-const fillButton = document.getElementById('fill-button');
 
 const grid = document.getElementById('grid');
 const mid = document.getElementById('middle');
 
 const sizeSlider = document.getElementById('size-slider');
 
-let currentBrush = blackBrush;
-let currentToggle = solidToggle;
+// write, eraser, and neither
+const pens = document.querySelectorAll('.pen-setting');
 
-let penType = 0; // 0 -> draw, 1 -> eraser, 2 -> off
+// palette icons -> black, user-selected color, random color
+const brushes = Array.from(document.querySelectorAll('.brush-toggle'));
 
-const BLACK_OBJ = new Color(0,0,0);
-let penColor = BLACK_OBJ;
-let bgColor = new Color(colorBackgroundInput.value);
+// dotted, solid
+const gridToggles = Array.from(document.querySelectorAll('.grid-toggle')).map(n => {
+  return {node: n, active: false};
+});
 
+// ----------------- Variables -------------------------------------------------
+// current palette selection
+let currentBrush = brushes[0];
+
+// index of current pen selection
+let penType = 0;
+
+// stores current color for pen and background
+let penColor = BLACK_COLOR;       //updated on palette swap
+let bgColor = new Color(colorBackgroundInput.value);  //updated when user presses apply
+
+// tracks current status of solid and drag-to-draw toggles
+let solidState = true;
+let dragToDraw = false;
+
+// the width and height of the main grid
 let size = sizeSlider.value;
 
-let solidState = true;
-let defaultDraw = true;
+// ----------------- Add Event Listeners ---------------------------------------
+brushes.forEach(b => b.addEventListener('click', handleBrushClick));
 
-let dottedGrid = false;
-let solidGrid = false;
-
-blackBrush.addEventListener('click', handleBlackBrushClick);
-colorBrush.addEventListener('click', handleColorBrushClick);
-rainbowBrush.addEventListener('click', handleRainbowBrushClick);
-
-solidToggle.addEventListener('click', handleSolidClick);
-dragToggle.addEventListener('click', handleDragClick);
-
-dottedGridToggle.addEventListener('click', handleDottedGridClick);
-solidGridToggle.addEventListener('click', handleSolidGridClick);
-
-colorBrushInput.addEventListener('input', handleColorBrushInput);
-colorBrushInput.addEventListener('change', handleColorBrushChange);
-
-resetButton.addEventListener('click', handleResetClick);
-applyButton.addEventListener('click', handleApplyClick);
-fillButton.addEventListener('click', handleFillClick);
-
-mid.addEventListener('click', togglePen);
-
-sizeSlider.addEventListener('input', handleSizeInput);
+gridToggles.forEach(t => t.node.addEventListener('click', handleGridToggle));
 
 pens.forEach(p => p.addEventListener('click', handlePenClick));
 
-function updateGridToggleDisplay() {
-  dottedGridToggle.style.color = (dottedGrid) ? '#000000' : '#808080';
-  solidGridToggle.style.color = (solidGrid) ? '#000000' : '#808080';
+colorBrushInput.addEventListener('input', e => brushes[1].style.color = e.target.value);
+colorBrushInput.addEventListener('change', e => penColor = new Color(e.target.value));
+
+mid.addEventListener('click', togglePen);
+
+sizeSlider.addEventListener('input', () => document.getElementById('size-text').textContent = sizeSlider.value);
+
+document.getElementById('solid-toggle').addEventListener('click', handleSolidClick);
+document.getElementById('drag-toggle').addEventListener('click', handleDragClick);
+
+document.getElementById('reset-button').addEventListener('click', handleResetClick);
+document.getElementById('apply-button').addEventListener('click', handleApplyClick);
+document.getElementById('fill-button').addEventListener('click', handleFillClick);
+
+// ----------------- Methods ---------------------------------------------------
+// Event -> null
+// toggles the selected grid, deactivates the other, updates the colors appropriately
+function handleGridToggle(e) {
+  gridToggles.forEach(t => {
+    t.active = (t.node == e.target && !t.active);
+    t.node.style.color = t.active ? BLACK : GREY;
+  });
+
+  updateGridlines();
 }
 
+// Updates the border of all squares to the active grid
 function updateGridlines() {
   let border = "none";
 
-  if (solidGrid) {
+  if (gridToggles[1].active) {
     border = `1px solid ${bgColor.getComplement().toString()}`;
-  } else if (dottedGrid) {
+  } else if (gridToggles[0].active) {
     border = `1px dotted ${bgColor.getComplement().toString()}`;
   }
 
   document.querySelectorAll('.square').forEach(s => s.style.border = border);
 }
 
-function handleDottedGridClick() {
-  solidGrid = false;
-  dottedGrid = !dottedGrid;
+// Event -> null
+// Updates palette selection, pen color, and places an appropriate border in the UI
+function handleBrushClick(e) {
+  currentBrush = e.target;
 
-  updateGridlines();
-  updateGridToggleDisplay();
+  if (brushes.indexOf(currentBrush) == 0) penColor = BLACK_COLOR;
+  else if (brushes.indexOf(currentBrush) == 1) {
+    colorBrushInput.click();
+    // in case the user never triggers change event
+    penColor = new Color(colorBrushInput.value);
+  };
+
+  applyBrushBorder();
 }
 
-function handleSolidGridClick() {
-  solidGrid = !solidGrid;
-  dottedGrid = false;
-
-  updateGridlines();
-  updateGridToggleDisplay();
+// removes the border from all palettes and re-applies the border to selected palette
+function applyBrushBorder() {
+  brushes.forEach(b => b.setAttribute('style', 'margin: 3px; border: 0px;'));
+  currentBrush.setAttribute('style', `margin: 0px; border: 3px solid ${GREY};`);
+  brushes[1].style.color = colorBrushInput.value;
 }
 
+// sets active pen icon to black color, rest to grey
+function updatePenDisplay() {
+  pens.forEach(p => p.style.color = GREY);
+  pens[penType].style.color = BLACK;
+}
+
+// Event -> null
+// updates the class of mid to show active pen cursor, updates penType and the UI
+// this method for direct selection via the right-hand menu
 function handlePenClick(e) {
   const penName = ['brush', 'eraser', 'none'];
 
@@ -110,30 +132,8 @@ function handlePenClick(e) {
   updatePenDisplay();
 }
 
-function handleSizeInput(e) {
-  document.getElementById('size-text').textContent = sizeSlider.value;
-}
-
-function handleApplyClick(e) {
-  clearGrid();
-
-  size = sizeSlider.value;
-  bgColor = new Color(colorBackgroundInput.value);
-
-  populateGrid(size);
-}
-
-function handleResetClick(e) {
-  clearGrid();
-
-  populateGrid(size);
-}
-
-function updatePenDisplay() {
-  pens.forEach(p => p.style.color = "#808080");
-  pens[penType].style.color = "#000000";
-}
-
+// updates the class of mid to show active pen cursor, updates penType and the UI
+// this method for inderect selection via click-toggle within the grid
 function togglePen() {
   const penName = ['brush', 'eraser', 'none'];
   mid.classList.remove(penName[penType]);
@@ -146,116 +146,30 @@ function togglePen() {
   updatePenDisplay();
 }
 
-function handleBlackBrushClick(e) {
-  currentBrush = blackBrush;
-  penColor = BLACK_OBJ;
+// clears the old grid, sets new background color and size, and popluates a new grid
+function handleApplyClick() {
+  clearGrid();
 
-  applyBrushBorder();
+  bgColor = new Color(colorBackgroundInput.value);
+  size = sizeSlider.value;
+
+  populateGrid();
 }
 
-function handleColorBrushClick(e) {
-  document.getElementById('brush-color').click();
-  currentBrush = colorBrush;
-  penColor = new Color(colorBrushInput.value);
-
-  applyBrushBorder();
-}
-
-function handleColorBrushChange(e) {
-  penColor = new Color(e.target.value);
-}
-
-function handleRainbowBrushClick(e) {
-  currentBrush = rainbowBrush;
-  applyBrushBorder();
-}
-
-function handleSolidClick(e) {
-  solidState = !solidState;
-  if (solidState) document.getElementById('solid-container').style.color = '#000000';
-  else document.getElementById('solid-container').style.color = '#808080';
-}
-
-function handleDragClick(e) {
-  defaultDraw = !defaultDraw;
-  const instructions = document.querySelector('.instruction-text');
-  if (defaultDraw) {
-    document.getElementById('drag-container').style.color = '#808080';
-    instructions.textContent = "Click anywhere within the center area to toggle pen mode";
-    mid.addEventListener('click', togglePen);
-  } else {
-    document.getElementById('drag-container').style.color = '#000000';
-    instructions.textContent = "Click and drag to draw";
-    mid.removeEventListener('click', togglePen);
-  }
-}
-
-function handleColorBrushInput(e) {
-  colorBrush.style.color = e.target.value;
-}
-
-function applyBrushBorder() {
-  brushes.forEach(b => b.setAttribute('style', 'margin: 3px; border: 0px;'));
-  currentBrush.setAttribute('style', 'margin: 0px; border: 3px solid #808080;')
-  colorBrush.style.color = colorBrushInput.value;
-}
-
-function getColor(oldColor) {
-  // solid eraser
-  if (penType == 1 && solidState) return bgColor;
-
-  // solid, ink
-  if (solidState) {
-    if (currentBrush !== rainbowBrush) return penColor;
-    return Color.getRandColor();
-  }
-
-  // all solid situations have been taken care of
-
-  // shaded eraser
-  if (penType == 1) {
-    return oldColor.getCloserColor(bgColor);
-  }
-
-  // if not rainbow
-  if (currentBrush !== rainbowBrush) {
-    return oldColor.getCloserColor(penColor);
-  }
-
-  return (oldColor.checkIfSame(bgColor)) ? Color.getRandColor() : oldColor;
-}
-
-function handleMouseEnter(e) {
-  if (penType == 2 || (!defaultDraw && e.buttons != 1)) return;
-  e.target.style.backgroundColor = getColor(new Color(e.target.style.backgroundColor)).toString();
-}
-
+// removes all child elements of the main grid
 function clearGrid() {
   while (grid.firstChild) grid.removeChild(grid.firstChild);
 }
 
-function handleMouseDown(e) {
-  if (!defaultDraw && penType != 2) {
-    e.target.style.backgroundColor = getColor(new Color(e.target.style.backgroundColor)).toString();
-  }
-}
-
-function handleFillClick(e) {
-  document.querySelectorAll('.square').forEach(s => {
-    if (new Color(s.style.backgroundColor).checkIfSame(bgColor)) {
-      s.style.backgroundColor = (currentBrush !== rainbowBrush) ? penColor.toString() : Color.getRandColor().toString();
-    }
-  });
-}
-
-function populateGrid(n) {
+// creates a square node, adds the appropriate css, clones and adds it to grid, and adds listeners
+function populateGrid() {
   const square = document.createElement('div');
   square.classList.add('square');
   square.style.backgroundColor = bgColor;
 
-  for (let i = 0; i < n**2; i++) grid.appendChild(square.cloneNode());
+  for (let i = 0; i < size**2; i++) grid.appendChild(square.cloneNode());
 
-  grid.style.gridTemplate = `repeat(${n}, 1fr) / repeat(${n}, 1fr)`;
+  grid.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
 
   const squares = grid.querySelectorAll('.square');
 
@@ -265,9 +179,85 @@ function populateGrid(n) {
   updateGridlines();
 }
 
+// clears the old grid and populates a new one based on same background color and size
+function handleResetClick() {
+  clearGrid();
 
+  populateGrid();
+}
+
+// swaps pen from solid to shader and updates the UI accordingly
+function handleSolidClick() {
+  solidState = !solidState;
+  document.getElementById('solid-container').style.color = (solidState) ? BLACK : GREY;
+}
+
+// swaps draw style, updates instructions and UI, and updates event listener in grid
+function handleDragClick() {
+  dragToDraw = !dragToDraw;
+
+  const instructions = document.getElementById('instruction-text');
+
+  if (!dragToDraw) {
+    document.getElementById('drag-container').style.color = GREY;
+    instructions.textContent = "Click anywhere within the center area to toggle pen mode";
+    mid.addEventListener('click', togglePen);
+  } else {
+    document.getElementById('drag-container').style.color = BLACK;
+    instructions.textContent = "Click and drag to draw";
+    instructions.style.textAlign = "center";
+    mid.removeEventListener('click', togglePen);
+  }
+}
+
+// Event -> null
+// when in drag-to-draw mode, colors or erases the current square according to current settings
+function handleMouseDown(e) {
+  if (dragToDraw && penType != 2) {
+    e.target.style.backgroundColor = getColor(new Color(e.target.style.backgroundColor)).toString();
+  }
+}
+
+// Color -> Color
+// given a color, calculate and return the new color based on current settings
+// assumes penType is pre-validated to not be 2
+function getColor(oldColor) {
+  // solid eraser -> always back to background color
+  if (penType == 1 && solidState) return bgColor;
+
+  // solid, ink
+  if (solidState) return (currentBrush !== brushes[2]) ? penColor : Color.getRandColor();
+
+  // all solid situations have been taken care of, rest are shaded
+
+  // eraser -> with shader on, eraser brings square closer to background color
+  if (penType == 1) return oldColor.getCloserColor(bgColor);
+
+  // if not rainbow -> with shader, brings square closer to pen color
+  if (currentBrush !== brushes[2]) return oldColor.getCloserColor(penColor);
+
+  // shading towards a random color not meaningful, so instead, randomly fill only blank squares
+  return (oldColor.checkIfSame(bgColor)) ? Color.getRandColor() : oldColor;
+}
+
+// Event -> null
+// validates that color should be applied and applies it based on calculations from getColor
+function handleMouseEnter(e) {
+  if (penType == 2 || (dragToDraw && e.buttons != 1)) return;
+  e.target.style.backgroundColor = getColor(new Color(e.target.style.backgroundColor)).toString();
+}
+
+// Event -> null
+// fills all empty squares based on current palette selection
+function handleFillClick() {
+  document.querySelectorAll('.square').forEach(s => {
+    if (new Color(s.style.backgroundColor).checkIfSame(bgColor)) {
+      s.style.backgroundColor = (currentBrush !== brushes[2]) ? penColor.toString() : Color.getRandColor().toString();
+    }
+  });
+}
+
+// initialise on page load
 applyBrushBorder();
-
-populateGrid(16);
-
+populateGrid();
 updatePenDisplay();
